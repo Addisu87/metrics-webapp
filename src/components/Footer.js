d9213c39e83aa7a53284bff6aa6c571bad6d3d60
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-07-28 00:58:57 +0300

    Add styling for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Footer() {
   return (
-    <div className="footer">
+    <FooterWrapper>
       <h3>
         Created by
         {' '}
-        <a href="https://github.com/Addisu87">Addisu Haile</a>
+        <Atag href="https://github.com/Addisu87">Addisu Haile</Atag>
       </h3>
-    </div>
+    </FooterWrapper>
   );
 }
 
 export default Footer;
+
+const FooterWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--darker);
+  margin: 1rem;
+  h3 {
+    color: white;
+  }
+`;
+
+const Atag = styled.a`
+  text-decoration: none;
+  font-style: italic;
+  color: rgb(236, 76, 138);
+`;
